55a7b8a1f22e8126119369089256ba95ab35a231
Filipe Rosa <filipe.rosa@cern.ch>
2023-01-22 10:08:17 +0000

    created vue component for the table with vehicle data

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,20 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import VehicleDataTable from './components/VehicleDataTable.vue'
 </script>
 
 <template>
-  <div>
+  <!-- <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  </div> 
+  <HelloWorld msg="Vite + Vue" /> 
+  -->
+  <VehicleDataTable />
 </template>
 
 <style scoped>
---
+++ client/src/components/VehicleDataTable.vue
@@ -0,0 +1,67 @@
+<template>
+<div id="vehicle-data-wrapper">
+    <div class="header">
+        <h2>Table of Vehicle Data</h2>
+    </div>
+    <div class="filters">
+
+    </div>
+    <div class="table-wrapper">
+        <table id="vehicle-data-table">
+            <thead>
+                <tr>
+                    <th>timestamp</th>
+                    <th>speed</th>
+                    <th>odometer</th>
+                    <th>soc</th>
+                    <th>elevation</th>
+                    <th>shift_state</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="datapoint in datapoints" v-bind:key="datapoint.id">
+                    <td>{{ datapoint.timestamp }}</td>
+                    <td>{{ datapoint.speed }}</td>
+                    <td>{{ datapoint.odometer }}</td>
+                    <td>{{ datapoint.soc }}</td>
+                    <td>{{ datapoint.elevation }}</td>
+                    <td>{{ datapoint.shift_state.id }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            selected_vehicle: "123",
+            vehicle_ids: [],
+            from: null,
+            to: null,
+            datapoints: [{"id":451,"speed":200,"state_of_charge":50,"shift_state_id":"D","vehicle_id":"123","timestamp":"2023-01-19T14:25:12.336794","odometer":49923.2,"elevation":130,"shift_state":{"id":"D","name":"Drive"}}]
+        };
+    },
+    methods: {
+        updateDatapoints(){
+            //TODO: call the API
+            return
+        },
+    },
+    computed: {
+        sortedDatapoints(){
+            //TODO: sort datapoints here
+            return datapoints
+        }
+    },
+    created() {
+        //TODO: load data from API (pre-fill vehicle id dropdown)
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
