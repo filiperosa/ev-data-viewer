5c37a3e7de70d145857b72553081d8d064d87e7d
Filipe Mendes Rosa <filipe.rosa@cern.ch>
2023-01-23 22:36:19 +0000

    added filtering by timestamp    added /vehicles endpoint to retrieve all the vehicle IDs

--- server/crud.py
+++ server/crud.py
@@ -9,17 +9,45 @@ from datetime import datetime
 import server.models as md
 
 
-def get_vehicle_data(db: Session, order: Union[asc, desc] = asc, page: int = 1, limit: int = 50):
+def get_vehicle_data(db: Session, from_date: datetime = None, to_date: datetime = None, order: Union[asc, desc] = asc, page: int = 1, limit: int = 50):
     """
     Get all vehicles datapoints
     """
 
-    return db.query(md.DataPoint).order_by(order(md.DataPoint.timestamp)).offset((page - 1) * limit).limit(limit).all()
+    query = db.query(md.DataPoint)
+
+    # retrieve datapoints from a specific datetime range
+    if from_date:
+        query = query.filter(md.DataPoint.timestamp >= from_date)
+    if to_date:
+        query = query.filter(md.DataPoint.timestamp <= to_date)
+
+    query = query.order_by(order(md.DataPoint.timestamp)).offset((page - 1) * limit).limit(limit)
+    
+    return query.all()
 
 
-def get_vehicle_data_by_id(db: Session, id: str, order: Union[asc, desc] = asc, page: int = 1, limit: int = 50):
+def get_vehicle_data_by_id(db: Session, id: str, from_date: datetime = None, to_date: datetime = None, order: Union[asc, desc] = asc, page: int = 1, limit: int = 50):
     """
     Get datapoints from a specific vehicle
     """
 
-    return db.query(md.DataPoint).filter(md.DataPoint.vehicle_id == id).order_by(order(md.DataPoint.timestamp)).offset((page - 1) * limit).limit(limit).all()
+    query = db.query(md.DataPoint).filter(md.DataPoint.vehicle_id == id)
+
+    # retrieve datapoints from a specific datetime range
+    if from_date:
+        query = query.filter(md.DataPoint.timestamp >= from_date)
+    if to_date:
+        query = query.filter(md.DataPoint.timestamp <= to_date)
+    
+    query = query.order_by(order(md.DataPoint.timestamp)).offset((page - 1) * limit).limit(limit)
+    
+    return query.all()
+
+
+def get_vehicles(db: Session, order: Union[asc, desc] = asc, page: int = 1, limit: int = 100):
+    """
+    Get all the vehicles
+    """
+
+    return db.query(md.Vehicle).order_by(order(md.Vehicle.id)).offset((page - 1) * limit).limit(limit).all()
--- server/routers/vehicles.py
+++ server/routers/vehicles.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import asc, desc
 from server.database import get_db
 from server import crud
+from datetime import datetime
 
 from typing import Union 
 from enum import Enum
@@ -15,18 +16,27 @@ class OrderByEnum(str, Enum):
 
 
 @router.get("/vehicle_data")
-def get_vehicle_data(db: Session = Depends(get_db), order: OrderByEnum = OrderByEnum.asc, page: int = 1, limit: int = 50):
+def get_vehicle_data(db: Session = Depends(get_db), from_date: datetime = None , to_date: datetime = None, order: OrderByEnum = OrderByEnum.asc, page: int = 1, limit: int = 50):
     """
     Get all vehicles datapoints
     """
 
-    return crud.get_vehicle_data(db, eval(order), page, limit)
+    return crud.get_vehicle_data(db, from_date, to_date, eval(order), page, limit)
 
 
 @router.get("/vehicle_data/{id}")
-def get_vehicle_data_by_id(id: str, db: Session = Depends(get_db), order: OrderByEnum = OrderByEnum.asc, page: int = 1, limit: int = 50):
+def get_vehicle_data_by_id(id: str, db: Session = Depends(get_db), from_date: datetime = None , to_date: datetime = None, order: OrderByEnum = OrderByEnum.asc, page: int = 1, limit: int = 50):
     """
     Get datapoints from a specific vehicle
     """
 
-    return crud.get_vehicle_data_by_id(db, id, eval(order), page, limit)
+    return crud.get_vehicle_data_by_id(db, id, from_date, to_date, eval(order), page, limit)
+
+
+@router.get("/vehicles")
+def get_vehicles(db: Session = Depends(get_db)):
+    """
+    Get all vehicles
+    """
+
+    return crud.get_vehicles(db)
